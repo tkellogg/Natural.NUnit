db1f57c10aca1b819fb2d28284a261e8de52b504
tkellogg <tkellogg@kpaonline.com>
2011-12-28 13:29:36 -0700

    At some point last week I wrote a bunch of code
    All the tests pass, so I'm wondering why I didn't commit. The only issue that I can see is that there's an empty test.

---
+++ src/BehavioralNUnit.Specs/when_doing_basic_collection_assert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BehavioralNUnit.Specs
+{
+	[TestFixture]
+	class when_doing_basic_collection_assert
+	{
+		[Test]
+		public void it_()
+		{
+			
+		}
+	}
+}
